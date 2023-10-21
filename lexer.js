0a73af4d624e2b7eab45527fd0d8b815838ea3ce
blackBuzo2601 <elianpro58@gmail.com>
2023-10-21 00:21:28 -0700

    Prueba Nueva creo qu estoy cerca de tener un buen resultado

--- lexer.js
+++ lexer.js
@@ -11,18 +11,36 @@ var querySpliteado=query.split(" "); //separar por espacios el Query
 const caracteresDiferentes=",=*" //Otros caracters que va a evaluar
 const posicionUltimaLetra=query.length-1;
 const ultimaLetra=query[posicionUltimaLetra];
-var letraActual="";
+var letra="";
 var palabraActual="";
+var formarPalabra="";
 
 
-
-for(let i=0;i<querySpliteado.length;i++){ //Bucle For que recorre cada separacion del query
-    palabraActual=querySpliteado[i]; //trabajar con cada palabra
+/*
+for(let i=0;i<querySpliteado.length;i++){ //Bucle For General que recorre cada separacion del query
+    palabraActual=querySpliteado[i];
 
     for(let j=0;j<palabraActual.length;j++){
+        letra=palabraActual[j];
         
-        
-        
-    }
     
-}//fin for general
+
+
+    
+
+
+}//fin for general  */
+
+
+var textoConComas= querySpliteado[2];
+var prueba=querySpliteado[2].length;
+var subtexto="";
+for(let j=0;j<prueba;j++){ 
+    letra=textoConComas[j];
+    formarPalabra=formarPalabra+letra;
+    if(caracteresDiferentes.includes(letra)){
+        subtexto=textoConComas.split(letra);
+        console.log(subtexto[0]);
+        console.log(subtexto[1]);
+    }
+}
