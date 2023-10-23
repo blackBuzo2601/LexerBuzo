b54658b1ad4ecafdcd362a395a04770cac871fb8
blackBuzo2601 <elianpro58@gmail.com>
2023-10-22 17:01:13 -0700

    El codigo funciona como debe funcionar en esta tarea. Lo que realiza es imprimir todas las palabras del query en saltos de linea diferentes de forma ordenada y tambien los caracteres especiales que acompañan dichas palabras. Es lo que debe realizar este codigo en este momento. Mas adelante en el repositorio LexerBuzoToken estará una copia de este mismo código, pero actualizado con la asignación de tokens con palabras reservadas de sql

--- lexer.js
+++ lexer.js
@@ -7,14 +7,14 @@
 /*  usamos el modulo fs de Node.js
     con require ('fs) se esta importando el modulo 
     para acceder a todas las funciones y metodos de fs. */
-    const fs = require ('fs'); 
+     
     /*readFile es un método de fs.
     UTF-8 el parámetro que indica que se desea leer el archivo en formato UTF-8. 
     (err, data) son las variables que usamos como parámetro de nuestro callBack.
     err: se utiliza para almacenar cualquier error que ocurra durante la operacion.
     data: se utiliza para almacenar los datos o el resultado de la operacion
     */
-    fs.readFile('sqlkeywords.txt','utf8', (err, data) => {
+    
     var query="'SELECT edad correo,sueldo FROM usuarios WHERE nombre='juan, pedro'";
     //variables necesarias
     var caracterVacio=" ";
@@ -29,20 +29,22 @@
         if(letraActual!=caracterVacio){ //primero evalua si el caracter SI es un caracter
 
             if(caracteresDiferentes.includes(letraActual)){ //evalua si es un caracterEspecial
-                if(formarPalabra==""){
-                    formarPalabra="";
-                    console.log(letraActual);
-                }else{
-                    console.log(formarPalabra);
-                    console.log(letraActual);
-                    formarPalabra="";
+                if(formarPalabra==""){ //en caso de que haya habido varios espacios en blanco
+                    formarPalabra="";   
+                    console.log(letraActual); //solo imprime la letraActual (que corresponde a un caracter Especial)
+
+                }else{ //si habia una palabra construida previamente entrará en esta condición
+                    console.log(formarPalabra); //imprimirá la palabra previamente construida
+                    console.log(letraActual);   //imprimirá después el caracter especial
+                    formarPalabra="";           //reiniciará la variable para volver a construir otra palabra
+                                                //en la siguiente iteracion.
                 }   
-            }else{
+            }else{ //si es un caracter comun y corriente, entrará aquí
                 formarPalabra=formarPalabra+letraActual; //va construyendo la palabra hasta encontrar un espacio
             }
         }//fin primer condicional
     
-        if(letraActual==caracterVacio){//después evalua si el caracter es vacio o " "
+        if(letraActual==caracterVacio){//aqui entra cuando el caracter es vacio.
             if(formarPalabra==""){ 
                 formarPalabra="";
             }else{ //esto se corre si formarPalabra tiene puras letras
@@ -56,3 +58,11 @@
     if(query[query.length-1]!=" "){ //evalua si el ultimo caracter del query es diferente de vacio. Para imprimir la palabra formada
         console.log(formarPalabra);
     }
+    
+
+
+
+
+
+
+
